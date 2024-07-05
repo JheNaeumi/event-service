330197e53294b2c78efebc03f3308f66523368a3
Jhe <jeremiahpasion56@gmail.com>
2024-07-05 22:59:38 +0800

    configure route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\EventController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/event')->name('index');
+Route::resource('event', EventController::class);
+   
