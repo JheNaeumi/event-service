c5d81cbc3af7e862a94cbf1af271dbc3361d7674
Jhe <jeremiahpasion56@gmail.com>
2024-07-08 22:59:05 +0800

    update controller

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -1,14 +1,14 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Event;
 use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
-    //
+    //TODO: Conroller using React FrontEnd
     public function index(){
-        
+        $event = Event::query() -> where('user_id', request()->user()->id) -> orderBy("created_at","desc")->paginate();
     }
     public function store(Request $request){
 
