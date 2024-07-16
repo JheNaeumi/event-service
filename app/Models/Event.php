849f571e6c542bbd4f1d9b257271a679320c8c9c
Jhe <jeremiahpasion56@gmail.com>
2024-07-16 21:45:13 +0800

    update Event entity

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -9,9 +9,15 @@ class Event extends Model
 {
     use HasFactory;
     
-    protected $fillable = [
-        'name',
-        'description',
-        'event_date',
-    ];
+    protected $fillable = ['name', 'description', 'event_date', 'user_id']; // Include user_id
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'event_user')->withTimestamps();
+    }
+
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
