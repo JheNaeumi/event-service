a80f68212cf9c779556718de44dc3b5fdf84fb3e
Jhe <jeremiahpasion56@gmail.com>
2024-07-18 23:19:00 +0800

    add Reminder Service

---
+++ app/Console/Commands/SendEventReminders.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\Event;
+use App\Notifications\EventReminderNotification;
+use Carbon\Carbon;
+
+class SendEventReminders extends Command
+{
+    protected $signature = 'events:send-reminders';
+    protected $description = 'Send reminders for events happening tomorrow';
+
+    public function handle()
+    {
+        $tomorrow = Carbon::tomorrow()->toDateString();
+
+        $events = Event::whereDate('event_date', $tomorrow)->get();
+
+        foreach ($events as $event) {
+            foreach ($event->users as $user) {
+                $user->notify(new EventReminderNotification($event));
+            }
+        }
+
+        $this->info('Event reminders sent successfully.');
+    }
+}
