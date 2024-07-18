e0986040683cce1221ba5a0b9dc3a9d147672a6c
Jhe <jeremiahpasion56@gmail.com>
2024-07-18 23:21:17 +0800

    add event notif and reminder

---
+++ app/Notifications/EventJoinedNotification.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use App\Models\Event;
+
+class EventJoinedNotification extends Notification
+{
+    use Queueable;
+
+    protected $event;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(Event $event)
+    {
+        $this->event = $event;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->line('You have successfully joined the event: ' . $this->event->name)
+                    ->line('Event Date: ' . $this->event->event_date)
+                    ->line('Thank you for participating!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ app/Notifications/EventReminderNotification.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use App\Models\Event;
+
+class EventReminderNotification extends Notification
+{
+    use Queueable;
+
+    protected $event;
+
+    public function __construct(Event $event)
+    {
+        $this->event = $event;
+    }
+
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->line('Reminder: You have an upcoming event tomorrow.')
+                    ->line('Event: ' . $this->event->name)
+                    ->line('Date: ' . $this->event->event_date)
+                    ->line('We look forward to seeing you there!');
+    }
+}
